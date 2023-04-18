2faeb90593d6013cad387e30695f44436f8678f9
Robin W <rejjkill.redunzl@gmail.com>
2023-04-18 19:38:35 +0200

    2

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,2 +1,25 @@
-public class Edge {
+public class Edge<T> {
+    private final T destination;
+    private final String name;
+    private final int weight;
+
+    public Edge(T destination, String name, int weight){
+        this.name = name;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public T getDestination() {
+        return destination;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String toString(){
+        return destination + ", " + name + ", " + weight;
+    }
 }
---
+++ src/main/java/Place.java
@@ -0,0 +1,8 @@
+public class Place extends Edge{
+    String name;
+
+    public Place(Place destination, String destinationName, int weight, String name){
+        super(destination, destinationName, weight);
+        this.name = name;
+    }
+}
