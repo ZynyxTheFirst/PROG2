11d28fbb2eaa5e7b22e61c417e8076d590b9c7ee
Robin W <rejjkill.redunzl@gmail.com>
2023-04-18 20:00:01 +0200

    3

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,7 +1,7 @@
 public class Edge<T> {
     private final T destination;
     private final String name;
-    private final int weight;
+    private int weight;
 
     public Edge(T destination, String name, int weight){
         this.name = name;
@@ -14,6 +14,13 @@ public class Edge<T> {
     }
     public int getWeight(){
         return weight;
+    }
+    public void setWeight(int weight){
+        if(weight < 0){
+            throw new IllegalArgumentException();
+        }else {
+            this.weight = weight;
+        }
     }
     public String getName(){
         return name;
