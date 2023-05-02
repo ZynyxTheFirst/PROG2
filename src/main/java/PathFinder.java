e5fae760457f102e2d9276001456c6be052cf042
Robin W <rejjkill.redunzl@gmail.com>
2023-05-02 19:01:18 +0200

    24

--- src/main/java/PathFinder.java
+++ src/main/java/PathFinder.java
@@ -1,13 +1,24 @@
+// PROG2 VT2023, Inlämningsuppgift, del 2
+// Grupp 031
+// Hanna Arrhenius haar9434
+// Erik Strandberg erst1916
+// Robin Westling rowe7856
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class PathFinder extends Application{
 
+
     public void start(Stage stage){
+
+        //Drop down menu
         MenuBar menuBar = new MenuBar();
         menuBar.setId("menu");
         Menu fileMenu = new Menu("File");
@@ -19,7 +30,21 @@ public class PathFinder extends Application{
         MenuItem exitMI = new MenuItem("Exit");
         fileMenu.getItems().addAll(newMapMI, openMI, saveMI, saveImageMI, exitMI);
         menuBar.getMenus().add(fileMenu);
-        Scene scene = new Scene(menuBar, 800, 600);
+
+        //Buttons
+        Button findPathButton = new Button("Find Path");
+        Button showConnectionButton = new Button("Show Connection");
+        Button newPlaceButton = new Button("New Place");
+        Button newConnectionButton = new Button("New Connection");
+        Button changeConnectionButton = new Button("Change Connection");
+        BorderPane borderPane = new BorderPane();
+        FlowPane buttons = new FlowPane();
+        buttons.getChildren().addAll(findPathButton, showConnectionButton, newPlaceButton, newConnectionButton, changeConnectionButton);
+        borderPane.setTop(menuBar);
+        borderPane.getChildren().add(buttons);
+
+        //Create scene
+        Scene scene = new Scene(borderPane, 800, 600);
         stage.setScene(scene);
         stage.setTitle("PathFinder");
         stage.show();
