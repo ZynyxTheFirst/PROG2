37c111c9987ab246d58d508318d1dfe58d1c3316
Robin W <rejjkill.redunzl@gmail.com>
2023-05-02 20:30:50 +0200

    hh

--- src/main/java/PathFinder.java
+++ src/main/java/PathFinder.java
@@ -4,19 +4,25 @@
 // Erik Strandberg erst1916
 // Robin Westling rowe7856
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 
 public class PathFinder extends Application{
 
-
+    private static final String imageUrl = "Z:\\University\\Prog2\\Grupparbete\\PROG2\\src\\main\\europa.gif";
     public void start(Stage stage){
+
+        ImageView imageView = new ImageView();
 
         //Drop down menu
         MenuBar menuBar = new MenuBar();
@@ -24,6 +30,14 @@ public class PathFinder extends Application{
         Menu fileMenu = new Menu("File");
         fileMenu.setId("menuFile");
         MenuItem newMapMI = new MenuItem("New Map");
+        newMapMI.setOnAction(event -> {
+            Image map = new Image(imageUrl);
+            imageView.setFitHeight(map.getHeight());
+            imageView.setFitWidth(map.getWidth());
+            imageView.setPreserveRatio(true);
+            imageView.setImage(map);
+            stage.sizeToScene();
+        });
         MenuItem openMI = new MenuItem("Open");
         MenuItem saveMI = new MenuItem("Save");
         MenuItem saveImageMI = new MenuItem("Save Image");
@@ -37,14 +51,17 @@ public class PathFinder extends Application{
         Button newPlaceButton = new Button("New Place");
         Button newConnectionButton = new Button("New Connection");
         Button changeConnectionButton = new Button("Change Connection");
-        BorderPane borderPane = new BorderPane();
-        FlowPane buttons = new FlowPane();
-        buttons.getChildren().addAll(findPathButton, showConnectionButton, newPlaceButton, newConnectionButton, changeConnectionButton);
-        borderPane.setTop(menuBar);
-        borderPane.getChildren().add(buttons);
+        VBox borderPane = new VBox();
+        HBox buttons = new HBox();
 
+        buttons.getChildren().addAll(findPathButton, showConnectionButton, newPlaceButton, newConnectionButton, changeConnectionButton);
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setPadding(new Insets(5));
+        buttons.setSpacing(10);
+
+        borderPane.getChildren().addAll(menuBar, buttons, imageView);
         //Create scene
-        Scene scene = new Scene(borderPane, 800, 600);
+        Scene scene = new Scene(borderPane);
         stage.setScene(scene);
         stage.setTitle("PathFinder");
         stage.show();
